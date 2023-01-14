9b451a3f240a631a5cd8953804114e35a8af2714
Berta Romano Fernandez <bertaromanofernandez@gmail.com>
2023-01-14 11:34:59 +0000

    Add endGame functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,11 @@
 const startQuiz = document.querySelector('#start');
 let finalScore = document.querySelector('#final-score');
 let timer = document.querySelector('#time');
-let secondsLeft = 90;
+let countdown;
+let secondsLeft = 60;
 let startScreen = document.querySelector('#start-screen');
 let quizScreen = document.querySelector('#questions');
+let endScreen = document.querySelector('#end-screen');
 let questionTitleEl = document.querySelector('#question-title');
 let choicesEl = document.querySelector('#choices');
 let questionIndex = 0;
@@ -12,7 +14,7 @@ let currentQuestion;
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-startQuiz.addEventListener('click', function(){
+startQuiz.addEventListener('click', function() {
     startScreen.style.display = 'none';
     showQuestion();
     timerCount();
@@ -31,57 +33,70 @@ function showQuestion() {
     choicesEl.appendChild(choicesOL);
 
     // choice refers to the key (a, b, c or d) and answer is the value (text answer)
-    for (const [choice, answer] of Object.entries(currentQuestion.choices)){
+    for (const [choice, answer] of Object.entries(currentQuestion.choices)) {
       
         let choiceLI = document.createElement("li");
         choiceLI.textContent = `${choice}: ${answer}`;
         choicesOL.appendChild(choiceLI);
-        choiceLI.dataset.choice = choice; // assigns the key 'choice' to the answers    
+        choiceLI.dataset.choice = choice; // assigns a dataset with the key 'choice' to the answers    
     }
 }
 
 // WHEN I answer a question
 // THEN I am presented with another question
-choicesEl.addEventListener('click', function(event){
+choicesEl.addEventListener('click', function(event) {
 
     let choiceLI = event.target;
     if (choiceLI.dataset.choice !== currentQuestion.correct) {
         // WHEN I answer a question incorrectly
         // THEN time is subtracted from the clock
-        secondsLeft -= 10;
+        if (secondsLeft < 10) {
+            secondsLeft = 0;
+        } else {
+            secondsLeft -= 10;
+        }
     }
 
-    questionIndex++;
-    showQuestion();
+    if (questions.length === questionIndex + 1){
+        endGame();
 
+    } else {
+        questionIndex++;
+        showQuestion();
+    }
 });
-
-
-// function wrongAnswer();
-    //deduct 10 secs
-    //if timer > 0 then showQuestion();
-
 
 // function to start timer countdown
 function timerCount() {
     
-    let countdown = setInterval(function() {
-        secondsLeft--;
-        timer.textContent = secondsLeft;
-        
-        if(secondsLeft <= 0) {
-            clearInterval(countdown);
+        countdown = setInterval(function() {
+
+        if (secondsLeft > 0) {
+            secondsLeft--;
         }
-        
-    }, 1000);
+   
+        timer.textContent = `${secondsLeft} seconds`;
+
+        if (secondsLeft === 1){
+            timer.textContent = `${secondsLeft} second`;
+        }
+
+        if(secondsLeft <= 0) {
+            endGame();
+        }
     
-    // finalScore.textContent = secondsLeft;
-    
+    }, 1000); 
 }
-
-
 
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
+function endGame() {
+    
+    clearInterval(countdown);
+    quizScreen.style.display = 'none';
+    endScreen.style.display = 'block';
+    finalScore.textContent = secondsLeft;
+
+}
 // WHEN the game is over
 // THEN I can save my initials and score
